a9f664fa676ca6c9bb891539c38a636fcad45187
thoera <erard.thomas@gmail.com>
2022-08-28 19:13:58 +0200

    Refactor the unit tests

--- tests/unit_tests/data/test_data.py
+++ tests/unit_tests/data/test_data.py
@@ -4,12 +4,10 @@ import numpy as np
 import pandas as pd
 
 
-def test_data_lng() -> None:
-    """Tests that the file with the weights for each question is correct."""
+def test_weights() -> None:
+    """Tests that the weights for each question are correct."""
     data = pd.read_csv("data/weights.csv")
     computed = (
-        data.groupby(["question_id", "variation_id", "choice_id"])
-        .sum()
-        .to_numpy()
+        data.groupby(["question_id", "item_id", "answer_id"]).sum().to_numpy()
     )
     assert np.all(computed == 1)
